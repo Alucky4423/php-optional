8220c939dc17bedc219148fb3d97615c9d66b115
Alucky4423 <a.lucky2510@gmail.com>
2017-12-30 22:26:56 +0900

    [update]

--- src/Optional.php
+++ src/Optional.php
@@ -2,4 +2,91 @@
 
 namespace Alucky4423;
 
-class Optional {}
+use Alucky4423\Exceptions\NullPointerException;
+
+class Optional
+{
+    private static $emptyInstance;
+    private $value;
+
+    /**
+     * @param $value
+     * @return Optional
+     * @throws NullPointerException
+     */
+    public static function of($value): Optional
+    {
+        if ($value === null)
+            throw new NullPointerException("value is null.");
+
+        return new self($value);
+    }
+
+
+    /**
+     * @param $value
+     * @return Optional
+     */
+    public static function ofNullable($value): Optional
+    {
+        if ($value === null)
+            return self::ofNull();
+
+        return new self($value);
+    }
+
+
+    /**
+     * @return Optional
+     */
+    public static function ofNull(): Optional
+    {
+        if (! isset(self::$emptyInstance))
+            self::$emptyInstance = new self(null);
+
+        return self::$emptyInstance;
+    }
+
+
+    /**
+     * Optional constructor.
+     * @param $value
+     */
+    private function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isPresent(): bool
+    {
+        return ($this->value !== null);
+    }
+
+
+    /**
+     * @param callable $callback
+     * @throws NullPointerException
+     */
+    public function ifPresent(callable $callback): void
+    {
+        if ($callback === null)
+            throw new NullPointerException('lambda is null.');
+
+        if ($this->isPresent())
+            $callback($this->value);
+    }
+
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        $type = gettype($this->value);
+        return "Optional<${type}>";
+    }
+}
--- tests/OptionalTest.php
+++ tests/OptionalTest.php
@@ -2,10 +2,90 @@
 
 use PHPUnit\Framework\TestCase;
 
+use Alucky4423\Optional;
+use Alucky4423\Exceptions\NullPointerException;
+
 class OptionalTest extends TestCase
 {
-    public function setUp()
+//    public function setUp()
+//    {
+//        //
+//    }
+
+
+    public function testOf()
     {
-        //
+        $actual = Optional::of('value');
+        $this->assertInstanceOf(Optional::class, $actual);
+    }
+
+
+    public function testOfThrowException()
+    {
+        $this->expectException(NullPointerException::class);
+        Optional::of(null);
+    }
+
+
+    public function ofNullableDataProvider()
+    {
+        return [
+            'null' => [null],
+            'string' => ['string'],
+            'int' => [1],
+            'object' => [new stdClass()]
+        ];
+    }
+
+    /**
+     * @param $value
+     * @dataProvider ofNullableDataProvider
+     */
+    public function testOfNullable($value)
+    {
+        $actual = Optional::ofNullable($value);
+        $this->assertInstanceOf(Optional::class, $actual);
+    }
+
+
+    public function testIsPresent()
+    {
+        $optional = Optional::of("test");
+        $this->assertTrue($optional->isPresent());
+        $optional = Optional::ofNullable(null);
+        $this->assertFalse($optional->isPresent());
+    }
+
+
+    public function ifPresent()
+    {
+        Optional::of("test")
+            ->ifPresent(function($value) {
+                $this->assertTrue(true);
+            });
+
+        Optional::ofNullable(null)
+            ->ifPresent(function($value) {
+                $this->assertNotTrue(true);
+            });
+    }
+
+
+    public function toStringDataProvider()
+    {
+        return [
+            'value of null.' => [null, 'Optional<NULL>'],
+            'value of string.' => ['string', 'Optional<string>'],
+        ];
+    }
+
+    /**
+     * @param $value
+     * @dataProvider toStringDataProvider
+     */
+    public function testToString($value, $expected)
+    {
+        $optional = Optional::ofNullable($value);
+        $this->assertEquals($expected, $optional->__toString(), $optional->__toString());
     }
 }
